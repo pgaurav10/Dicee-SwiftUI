6c6f8ce20390b6a96cc1eef68c1bf04dae90f2aa
Gaurav <gauravpatil24121997@gmail.com>
2022-01-21 11:58:29 -0600

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Gaurav Patil on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
